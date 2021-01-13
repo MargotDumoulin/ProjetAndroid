2bcac1867eec265eb873e8502a27fc274dee3ae8
tatouvine <erwin.morel5@etu.univ-lorraine.fr>
2021-01-13 14:04:25 +0100

    :bug: fix bug button validate in new product

--- TD1/app/src/main/java/com/example/td1/NewProductFragment.java
+++ TD1/app/src/main/java/com/example/td1/NewProductFragment.java
@@ -2,6 +2,7 @@ package com.example.td1;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -26,6 +28,7 @@ public class NewProductFragment extends Fragment {
     private static final int MAIN_CATALOGUE = 1;
 
     private View root;
+    private Button createButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -41,41 +44,47 @@ public class NewProductFragment extends Fragment {
         this.priceEditText = this.root.findViewById(R.id.productPriceEditText);
         this.titleEditText = this.root.findViewById(R.id.productTitleEditText);
         this.descriptionEditText = this.root.findViewById(R.id.productDescriptionEditText);
+        this.createButton =this.root.findViewById(R.id.createButton);
+        this.createButton.setOnClickListener(this::onClickAddProduct);
     }
 /**
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }**/
-/**
-    @Override
-    public void onBackPressed() {
-        Log.d("MainActivity", "test");
-        this.onClickGoBack(null);
-    }**/
+ @Override public boolean onOptionsItemSelected(MenuItem item)
+ {
+ switch (item.getItemId()) {
+ case android.R.id.home:
+ onBackPressed();
+ return true;
+ default:
+ return super.onOptionsItemSelected(item);
+ }
+ }**/
+    /**
+     * @Override public void onBackPressed() {
+     * Log.d("MainActivity", "test");
+     * this.onClickGoBack(null);
+     * }
+     **/
 
     public void onClickAddProduct(View v) {
         if (!this.titleEditText.getText().toString().equals("") && !this.descriptionEditText.getText().toString().equals("") && !this.priceEditText.getText().toString().equals("")) {
-            Intent intent = new Intent(NewProductFragment.this.getContext(), VenteCatalogueFragment.class);
-            intent.putExtra("newProduct", new Produit(5, 0, Double.parseDouble(String.valueOf(this.priceEditText.getText())), "img", String.valueOf(this.descriptionEditText.getText()), String.valueOf(this.titleEditText.getText()), new ArrayList<>()));
-            intent.putExtra("id_categ", 0);
-            intent.putExtra("requestCode", MAIN_CATALOGUE);
-            startActivity(intent);
-            //this.finish();
+            Bundle bundle = new Bundle();
+            bundle.putInt("id_categ", 0);
+            bundle.putSerializable("newProduct", new Produit(5, 0, Double.parseDouble(String.valueOf(this.priceEditText.getText())), "img", String.valueOf(this.descriptionEditText.getText()), String.valueOf(this.titleEditText.getText()), new ArrayList<>()));
+            Navigation.findNavController(v).navigate(R.id.action_newProductFragment_to_venteCatalogueFragment,bundle);
+            /**
+             Intent intent = new Intent(NewProductFragment.this.getContext(), VenteCatalogueFragment.class);
+             intent.putExtra("newProduct", new Produit(5, 0, Double.parseDouble(String.valueOf(this.priceEditText.getText())), "img", String.valueOf(this.descriptionEditText.getText()), String.valueOf(this.titleEditText.getText()), new ArrayList<>()));
+             intent.putExtra("id_categ", 0);
+             intent.putExtra("requestCode", MAIN_CATALOGUE);
+             startActivity(intent);
+             this.finish();**/
         } else {
             Toast.makeText(this.getContext(), getString(R.string.fill_all_fields), Toast.LENGTH_LONG).show();
         }
     }
 
 /**
-    public void onClickGoBack (View v) {
-        this.finish();
-    }**/
+ public void onClickGoBack (View v) {
+ this.finish();
+ }**/
 }
