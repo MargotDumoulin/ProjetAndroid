8a214ebcc86c33a6d027456777ddfb0ef4089b24
tatouvine <erwin.morel5@etu.univ-lorraine.fr>
2021-01-19 13:07:52 +0100

    :construction:add textView

---
+++ TD1/app/src/main/java/com/example/td1/ui/mentionLegales/MentionLegalesFragment.java
@@ -0,0 +1,24 @@
+package com.example.td1.ui.mentionLegales;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.td1.R;
+
+public class MentionLegalesFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_mention_legales, container, false);
+        return root;
+    }
+}
