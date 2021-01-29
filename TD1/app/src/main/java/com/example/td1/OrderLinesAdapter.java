93fa23650228a0d0fb759f8584476fd7e132903e
Snorloux <margot.dumoulin18@gmail.com>
2021-01-29 10:34:47 +0100

    :bug: able to scroll listview in landscape (last order)

--- TD1/app/src/main/java/com/example/td1/OrderLinesAdapter.java
+++ TD1/app/src/main/java/com/example/td1/OrderLinesAdapter.java
@@ -22,7 +22,6 @@ public class OrderLinesAdapter extends ArrayAdapter<OrderLine> {
     }
 
     public View getView(int index, View convertView, ViewGroup parent) {
-        Log.e("TEST", "on rentre dans getView");
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_list_order_lines, parent, false);
         }
